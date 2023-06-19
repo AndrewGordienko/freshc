19eead8d270239625d1684bbcee4748799d7c273
Andrew <andrew.gordienko05@gmail.com>
2023-06-19 15:38:36 -0400

    working on stuff

--- add.cpp
+++ add.cpp
@@ -6,14 +6,14 @@
 
 class Floor {
 public:
-    Floor(b2World& world) {
+    Floor(b2World& world, float y) {
         b2BodyDef groundBodyDef;
-        groundBodyDef.position.Set(0.0f, 400.0f);
+        groundBodyDef.position.Set(40.0f, y);  // Adjusted position
         groundBody = world.CreateBody(&groundBodyDef);
 
         b2PolygonShape groundBox;
-        groundBox.SetAsBox(650.0f, 10.0f);
-        
+        groundBox.SetAsBox(40.0f, 1.0f);  
+
         b2FixtureDef groundFixtureDef;
         groundFixtureDef.shape = &groundBox;
         groundBody->CreateFixture(&groundFixtureDef);
@@ -30,8 +30,8 @@ private:
 class Shape {
 public:
     Shape(b2World& world) {
-        const float shapeX = 100.0f; // X position relative to the center of the screen
-        const float shapeY = 100.0f; // Y position relative to the center of the screen
+        const float shapeX = 20.0f;  
+        const float shapeY = 20.0f;  // Adjusted position
 
         b2BodyDef bodyDef;
         bodyDef.type = b2_dynamicBody;
@@ -39,11 +39,11 @@ public:
         box = world.CreateBody(&bodyDef);
 
         int numSides = std::rand() % 5 + 4; // Random number of sides between 4 and 8
-        float distance = 30.0f * std::sin(b2_pi / numSides);
+        float distance = 20.0f * std::sin(b2_pi / numSides);  // Increased size
         vertices.reserve(numSides);
         for (int i = 0; i < numSides; i++) {
-            float x = distance * std::cos(2 * b2_pi / numSides * i) + getRandom(-10.0f, 10.0f);
-            float y = distance * std::sin(2 * b2_pi / numSides * i) + getRandom(-10.0f, 10.0f);
+            float x = distance * std::cos(2 * b2_pi / numSides * i);
+            float y = distance * std::sin(2 * b2_pi / numSides * i);
             vertices.emplace_back(x, y);
         }
 
@@ -53,6 +53,11 @@ public:
         boxFixture.density = 1.0f;
         boxFixture.friction = 0.3f;
         box->CreateFixture(&boxFixture);
+    }
+
+
+    b2Body* GetBoxBody() {
+        return box;
     }
 
     const b2Vec2* GetVertices() const {
@@ -67,41 +72,33 @@ private:
     b2Body* box;
     b2FixtureDef boxFixture;
     std::vector<b2Vec2> vertices;
-
-    float getRandom(float min, float max) {
-        std::random_device rd;
-        std::mt19937 gen(rd());
-        std::uniform_real_distribution<float> dis(min, max);
-        return dis(gen);
-    }
 };
 
-
+b2Vec2 box2dToScreen(const b2Vec2& input, int windowHeight) {
+    float scale = 20.0f; 
+    b2Vec2 output;
+    output.x = scale * input.x;
+    output.y = windowHeight - scale * input.y;
+    return output;
+}
 
 void drawBox() {
     const int SCREEN_WIDTH = 800;
     const int SCREEN_HEIGHT = 800;
 
-    // Create the Box2D world and setup physics simulation
-    b2Vec2 gravity(0.0f, 0.1f); // Weaker gravity
+    b2Vec2 gravity(0.0f, -9.8f); 
     b2World world(gravity);
 
-    // Create the Floor object
-    Floor floor(world);
+    Floor floor(world, SCREEN_HEIGHT / (2.0f * 20.0f)); // Halfway down the screen
 
-    // Create the Shape object
     Shape shape(world);
 
-    // Initialize SDL
     SDL_Init(SDL_INIT_VIDEO);
 
-    // Create SDL window
     SDL_Window* window = SDL_CreateWindow("Box2D Physics Simulation", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
 
-    // Create SDL renderer
     SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
-    // Main rendering loop
     bool running = true;
     while (running) {
         SDL_Event event;
@@ -111,45 +108,43 @@ void drawBox() {
             }
         }
 
-        // Perform physics simulation steps
-        float32 timeStep = 1.0f / 60.0f; // Use a smaller time step
+        float32 timeStep = 1.0f / 60.0f; 
         int32 velocityIterations = 6;
         int32 positionIterations = 2;
         world.Step(timeStep, velocityIterations, positionIterations);
 
-        // Clear the renderer
         SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
         SDL_RenderClear(renderer);
 
-        // Draw the ground (gray color)
         SDL_SetRenderDrawColor(renderer, 128, 128, 128, 255);
         SDL_Rect groundRect;
-        groundRect.x = static_cast<int>(floor.GetGroundBody()->GetPosition().x - SCREEN_WIDTH / 2.0f);
-        groundRect.y = static_cast<int>(floor.GetGroundBody()->GetPosition().y - 10.0f);
+        b2Vec2 floorPosition = box2dToScreen(floor.GetGroundBody()->GetPosition(), SCREEN_HEIGHT);
+        groundRect.x = static_cast<int>(floorPosition.x - SCREEN_WIDTH / 2.0f);
+        groundRect.y = static_cast<int>(floorPosition.y - 10.0f);
         groundRect.w = SCREEN_WIDTH;
         groundRect.h = 20;
         SDL_RenderFillRect(renderer, &groundRect);
 
-        // Draw the shape (white color)
         SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+        b2Vec2 shapePosition = box2dToScreen(shape.GetBoxBody()->GetPosition(), SCREEN_HEIGHT);
         const b2Vec2* vertices = shape.GetVertices();
         int numVertices = shape.GetNumVertices();
         for (int i = 0; i < numVertices; ++i) {
             int nextIndex = (i + 1) % numVertices;
+            b2Vec2 vertex1 = box2dToScreen(vertices[i] + shape.GetBoxBody()->GetPosition(), SCREEN_HEIGHT);
+            b2Vec2 vertex2 = box2dToScreen(vertices[nextIndex] + shape.GetBoxBody()->GetPosition(), SCREEN_HEIGHT);
+
             SDL_RenderDrawLine(renderer,
-                static_cast<int>(vertices[i].x + SCREEN_WIDTH / 2.0f),
-                static_cast<int>(vertices[i].y + SCREEN_HEIGHT / 2.0f),
-                static_cast<int>(vertices[nextIndex].x + SCREEN_WIDTH / 2.0f),
-                static_cast<int>(vertices[nextIndex].y + SCREEN_HEIGHT / 2.0f));
+                static_cast<int>(vertex1.x),
+                static_cast<int>(vertex1.y),
+                static_cast<int>(vertex2.x),
+                static_cast<int>(vertex2.y));
         }
 
-        // Update the screen
         SDL_RenderPresent(renderer);
     }
 
-    // Cleanup
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
 }
-
